4061c43910ace41655da1d09e8e3bbee9c66ff3b
nadun96 <nadunudaraka@gmail.com>
2025-04-17 17:46:11 +0530

    create and get

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/create", (req, res) => {
+  const { id, name, address, salary } = req.body;
+  db.query(
+    "INSERT INTO customer (id, name, address , salary) VALUES (?, ?, ?, ?)",
+    [id, name, address, salary],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error inserting data");
+      } else {
+        res.status(201).send({ message: "created", result: result });
+      }
+    }
+  );
+});
+
+app.get("/find-all", (req, res) => {
+  db.query("SELECT * FROM customer", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error fetching data");
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
