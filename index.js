a66e85fba42c8dc1ec09c2573fb4de25885d1f11
nadun96 <nadunudaraka@gmail.com>
2025-04-18 11:32:51 +0530

    full crud

--- index.js
+++ index.js
@@ -37,6 +37,52 @@ app.post("/create", (req, res) => {
   );
 });
 
+app.get("/find/:id", (req, res) => {
+  const { id } = req.params.id;
+  const query = "SELECT * FROM customer WHERE id = ?";
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error fetching data");
+    } else {
+      res.status(200).send({ message: "found", result: result });
+    }
+  });
+});
+
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params.id;
+  const { name, address, salary } = req.body;
+  const query =
+    "UPDATE customer SET name = ?, address = ?, salary = ? WHERE id = ?";
+  db.query(query, [name, address, salary, id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error updating data");
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).send("No record found with this ID");
+    }
+    res.status(200).send({ message: "updated", result: result });
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params.id;
+  const query =
+    "DELETE FROM customer WHERE id = ?";
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error updating data");
+    }
+    if (result.affectedRows === 0) {
+      res.status(404).send("No record found with this ID");
+    }
+    res.status(204).send({ message: "deleted", result: result });
+  });
+});
+
 app.get("/find-all", (req, res) => {
   db.query("SELECT * FROM customer", (err, result) => {
     if (err) {
